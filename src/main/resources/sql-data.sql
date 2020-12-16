d6ea5d302927e68e4b722ed50b07a3b1ed2b0536
Nj147 <47452401+Nj147@users.noreply.github.com>
2020-12-16 14:10:43 +0000

    Added dummy data to sql-data file

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,2 +1,5 @@
-INSERT INTO `ims`.`customers` (`firstName`, `lastName`) VALUES ('Nathan', 'Jackson');
-INSERT INTO `ims`.orders(`customer_id`) VALUES(1);
+USE `ims`;
+INSERT INTO `customers` (`firstName`, `lastName`) VALUES ('jordan', 'harrison');
+INSERT INTO `orders` (`customer_id`) VALUES ('1');
+INSERT INTO `items` (`itemName`, `stockCount`, `price`) VALUES ('Item 1', 100, 99.99);
+INSERT INTO `orders_items` (`order_id`, `item_id`, `quantity`) VALUES (1, 1, 2);
