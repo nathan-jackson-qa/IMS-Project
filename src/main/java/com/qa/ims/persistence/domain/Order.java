e2230254f88a5efc637a41b5d8fa864ca76e6cf5
Nj147 <47452401+Nj147@users.noreply.github.com>
2021-01-06 11:45:29 +0000

    Added in order testing for all functions, to 100% coverage

--- src/main/java/com/qa/ims/persistence/domain/Order.java
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -3,6 +3,7 @@ package com.qa.ims.persistence.domain;
 import java.util.HashMap;
 
 public class Order {
+
 	long order_id;
 	long customer_id;
 	double price;
@@ -69,6 +70,24 @@ public class Order {
 		itemsOrdered.remove(itemID);
 	}
 
+	
+	
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getDateOrdered() {
+		return dateOrdered;
+	}
+
+	public void setDateOrdered(String dateOrdered) {
+		this.dateOrdered = dateOrdered;
+	}
+
 	@Override
 	public String toString() {
 		return "Order ID: " + order_id + ", Customer ID: " + customer_id + ", Date Ordered: " + dateOrdered;
@@ -78,6 +97,47 @@ public class Order {
 		return "Order ID: " + order_id + ", Total Price: £" + price;
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (customer_id ^ (customer_id >>> 32));
+		result = prime * result + ((dateOrdered == null) ? 0 : dateOrdered.hashCode());
+		result = prime * result + ((itemsOrdered == null) ? 0 : itemsOrdered.hashCode());
+		result = prime * result + (int) (order_id ^ (order_id >>> 32));
+		long temp;
+		temp = Double.doubleToLongBits(price);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (customer_id != other.customer_id)
+			return false;
+		if (dateOrdered == null) {
+			if (other.dateOrdered != null)
+				return false;
+		} else if (!dateOrdered.equals(other.dateOrdered))
+			return false;
+		if (itemsOrdered == null) {
+			if (other.itemsOrdered != null)
+				return false;
+		} else if (!itemsOrdered.equals(other.itemsOrdered))
+			return false;
+		if (order_id != other.order_id)
+			return false;
+		if (Double.doubleToLongBits(price) != Double.doubleToLongBits(other.price))
+			return false;
+		return true;
+	}
 	
 	
 }
